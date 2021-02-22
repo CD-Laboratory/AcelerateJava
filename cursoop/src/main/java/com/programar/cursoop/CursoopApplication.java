bfefa940e254d99472e9327e9fd8c7b2a947e0e0
Jess <silveira.jess26@gmail.com>
2021-02-22 19:23:06 -0300

    aula03_part01
    Criando aplicação spring

---
+++ cursoop/src/main/java/com/programar/cursoop/CursoopApplication.java
@@ -0,0 +1,13 @@
+package com.programar.cursoop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursoopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursoopApplication.class, args);
+	}
+
+}
