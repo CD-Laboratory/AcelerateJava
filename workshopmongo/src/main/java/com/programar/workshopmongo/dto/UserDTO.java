f2160b7a8702947ae03ce3c610e0087b65800112
Jess <silveira.jess26@gmail.com>
2021-04-20 20:09:06 -0300

    mongoDB_aula01_part2

---
+++ workshopmongo/src/main/java/com/programar/workshopmongo/dto/UserDTO.java
@@ -0,0 +1,50 @@
+package com.programar.workshopmongo.dto;
+
+import java.io.Serializable;
+
+import com.programar.workshopmongo.domain.User;
+
+public class UserDTO implements Serializable{
+
+	//atributos
+	private static final long serialVersionUID = 1L;
+	
+	private String id;
+	private String name;
+	private String email;
+	
+	//construtores
+	public UserDTO() {}
+	
+	public UserDTO(User obj) {
+		id= obj.getId();
+		name = obj.getName();
+		email = obj.getEmail();
+	}
+
+	//Getters and Setters
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+}
