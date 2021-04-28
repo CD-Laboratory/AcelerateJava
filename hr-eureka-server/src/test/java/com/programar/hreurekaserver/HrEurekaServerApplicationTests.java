9d07f17ed118ddae6c6e48736b5f0fc83f24f847
Jess <silveira.jess26@gmail.com>
2021-04-27 22:24:10 -0300

    microservice_fase2_part1
    configuração do eureka

---
+++ hr-eureka-server/src/test/java/com/programar/hreurekaserver/HrEurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.programar.hreurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrEurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
