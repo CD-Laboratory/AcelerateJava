cff0f6a1bd9460e1360735b283e523d4748fde05
Jess <silveira.jess26@gmail.com>
2021-04-28 22:18:34 -0300

    microservice_fase2_part1
    Eureka, Ribbon, Hystrix

--- hr-payroll/src/main/java/com/programar/hrpayroll/resources/PaymentResource.java
+++ hr-payroll/src/main/java/com/programar/hrpayroll/resources/PaymentResource.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import com.programar.hrpayroll.entities.Payment;
 import com.programar.hrpayroll.services.PaymentService;
 
@@ -17,9 +18,16 @@ public class PaymentResource {
 	@Autowired
 	private PaymentService service;
 	
+	@HystrixCommand(fallbackMethod = "getPaymentAlternative")
 	@GetMapping(value = "/{workerId}/days/{days}")
 	public ResponseEntity<Payment> getPayment(@PathVariable long workerId, @PathVariable Integer days){
 		Payment payment = service.getPayment(workerId, days);
 		return ResponseEntity.ok(payment);
 	}
+
+	public ResponseEntity<Payment> getPaymentAlternative(long workerId, Integer days){
+		Payment payment = new Payment("Bran", 400.0, days);
+		return ResponseEntity.ok(payment);
+	}
+	
 }
